06d21165f03c054a143746feec0dd042e7c45d6e
Benjamin Bengfort <benjamin@bengfort.com>
2016-07-18 22:28:42 -0400

    annotate documents

--- corpus/views.py
+++ corpus/views.py
@@ -22,11 +22,13 @@ from django.views.generic import DetailView
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
-from corpus.models import Document, Annotation
+from corpus.models import Document, Annotation, Label
 from corpus.serializers import DocumentSerializer
+from corpus.serializers import AnnotationSerializer
 from corpus.exceptions import CorpusException
 
 
@@ -38,6 +40,25 @@ class DocumentDetail(DetailView):
 
     model = Document
     template_name = 'corpus/document.html'
+    context_object_name = 'document'
+    labels_parent_name  = 'USA Political Parties'
+
+
+    def get_context_data(self, **kwargs):
+        context = super(DocumentDetail, self).get_context_data(**kwargs)
+
+        # Add user-specific parameters
+        document   = context['document']
+        annotation = document.annotations.filter(user=self.request.user).first()
+        context['annotation'] = annotation
+
+        # Add label-specific parameters
+        # TODO Do not hard code the parent into the class!
+        context['labels'] = Label.objects.filter(
+            parent__name = self.labels_parent_name
+        )
+
+        return context
 
 ##########################################################################
 ## API HTTP/JSON Views
@@ -83,3 +104,25 @@ class DocumentViewSet(viewsets.ModelViewSet):
 
         except CorpusException as e:
             raise ValidationError(str(e))
+
+    @detail_route(methods=['post'], permission_classes=[IsAuthenticated])
+    def annotate(self, request, pk=None):
+        """
+        Allows the specification of an annotation (label) for the given
+        document. Note that a user can only have one label associated with
+        one document, for the time being.
+        """
+
+        # Get the document of the detail view and deserialize data
+        document   = self.get_object()
+        serializer = AnnotationSerializer(
+            data=request.data,
+            context={'request': request, 'document': document}
+        )
+
+        # Validate the serializer and save the annotation
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # Return the response
+        return Response(serializer.data)
